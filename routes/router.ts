05c7051dee7bcf6c31a6451afed18a5d96fa3e50
Cesar Riojas <invence.saltillo@gmail.com>
2018-12-04 15:28:20 -0600

    Fin seccion 5

--- routes/router.ts
+++ routes/router.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from "express";
+import Server from '../classes/server';
 
 const router = Router();
 
@@ -16,6 +17,14 @@ router.post('/mensajes', ( req: Request, res: Response ) => {
 
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
+    const server = Server.instance;
+
+    const payload = {
+        de,
+        cuerpo
+    }
+
+    server.io.emit( 'mensaje-nuevo', payload);
 
     res.json({
         ok: true,
@@ -30,6 +39,19 @@ router.post('/mensajes/:id', ( req: Request, res: Response ) => {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
     const id = req.params.id;
+
+    const payload = {
+        de,
+        cuerpo
+    }
+
+    const server = Server.instance;
+
+    // Mensaje a un usuario segun ID
+    server.io.in( id ).emit( 'mensaje-privado', payload);
+
+    // Mensaje a todos los usuarios
+    // server.io.emit( 'mensaje-privado', payload);
 
     res.json({
         ok: true,
